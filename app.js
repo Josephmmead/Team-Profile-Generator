691d4fcf91064ce621666af70c48fd781baf17b8
Joseph Mead <josephmmead@gmail.com>
2020-10-30 21:48:39 -0400

    added classes, logic, and dependencies

--- app.js
+++ app.js
@@ -10,10 +10,188 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const teamMembers = [];
+const arrayId = [];
 
+
+
+    function  createTeam(){
+        // inquirer to ask which type of employee you want to create and runs the relevant function
+
+        console.log("createTeam");
+        inquirer.prompt([
+            {
+                type:"list",
+                name: "team",
+                message: "Would you like to add a member to your team?",
+                choices: ["Engineer", "Manager", "Intern", "Done"]
+            }
+            
+        ]).then(answer => {
+            switch(answer.team) {
+                case "Engineer":
+                 createEngineer()
+                  break;
+                case "Manager":
+                  // code block
+                  break;
+                case "Intern":
+                    // code block
+                    break;  
+                default:
+                  buildTeam()
+              }
+        })
+    };
+
+
+    function createManager(){
+
+    };
+    function createIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is your intern's name",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid name"
+                }
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What is your intern's ID?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid ID"
+                }
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is your intern's email?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid email"
+                }
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What is your intern's School?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid school"
+                }
+            }
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+            teamMembers.push(intern)
+            arrayId.push(answers.internId)
+            // run a function here that creates the entire "team" prompting you to create another employee
+        })
+    };
+    
+
+    function createEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is your engineer's name",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid name"
+                }
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is your employee's ID?",
+                validate: answer => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+
+                    // console.log(pass);
+
+                    if(pass) {
+                        return true
+                    }
+
+                    return "Please enter a valid ID"
+                }
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your employee's email?",
+                validate: answer => {
+                    const pass = answer.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+                    if(pass) {
+                        return true
+                    }
+
+                    return "Please enter a valid email"
+                }
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is your employee's Github?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid github"
+                }
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
+            teamMembers.push(engineer)
+            arrayId.push(answers.engineerId)
+            createTeam();
+            // run a function here that creates the entire "team" prompting you to create another employee
+        })
+    }
+
+    function buildTeam() {
+        // Create the output directory if the output path doesn't exist
+        if (!fs.existsSync(OUTPUT_DIR)) {
+          fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+      };
+
+      
+function appMenu() {
+
+    createTeam();
+
+   
+
+
+}
+
+appMenu()
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
